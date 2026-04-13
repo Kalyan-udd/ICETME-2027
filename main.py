f5c6598b4871b9908173572c5faaf9e181ca9059
Kalyan_udd <kalyansaiuddagiri22@gmail.com>
2026-04-13 16:31:52 +0530

    changes in some formats of files.

--- main.py
+++ main.py
@@ -8,30 +8,10 @@ app.mount("/templates/static", StaticFiles(directory="./templates/static"), name
 
 templates = Jinja2Templates(directory='templates')
 
-posts: list[dict] = [
-
-    {
-        "id" : 1,
-        "name" : "kalyan",
-        "title" : "IC engines.",
-        "description" : "research on IC engines for optimal fuel efficiency",
-        "date_of_posted" : "20-april-2026"
-    },
-
-    {
-        "id" : 2,
-        "name" : "rohit",
-        "title" : "diesel engines",
-        "description" : "research on diesel engines for optimal fuel efficiency",
-        "date_of_posted" : "26-april-2026"
-    }
-
-]
-
 @app.get("/")
 async def home(request: Request):
     return templates.TemplateResponse(request, "home.html")
 
-@app.get("/api/posts")
-async def get_posts():
-    return posts
+@app.post("/")
+async def registration(request: Request):
+    return templates.TemplateResponse(request, "registration.html")
