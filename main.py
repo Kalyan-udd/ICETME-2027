3c209c595c52d9eb933bbb34ef1d15b9b422b8cc
Jithendranadh Uddagiri <jithendranadh.uddagiri@triotech.co.in>
2026-04-19 19:23:55 +0530

    move static to public/ for Vercel CDN, fix Lambda bundle size

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ app = FastAPI()
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-app.mount("/static", StaticFiles(directory=os.path.join(BASE_DIR, "static")), name='static')
+static_dir = os.path.join(BASE_DIR, "public", "static")
+if os.path.exists(static_dir):
+    app.mount("/static", StaticFiles(directory=static_dir), name='static')
 
 templates = Jinja2Templates(directory=os.path.join(BASE_DIR, 'templates'))
 
