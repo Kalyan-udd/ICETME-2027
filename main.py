fdea866a260d64d008e3964fc77cff6e5a2c8114
Jithendranadh Uddagiri <jithendranadh.uddagiri@triotech.co.in>
2026-04-19 18:41:52 +0530

    fix: use absolute paths for templates and static dirs

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
+import os
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name='static')
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-templates = Jinja2Templates(directory='templates')
+app.mount("/static", StaticFiles(directory=os.path.join(BASE_DIR, "static")), name='static')
+
+templates = Jinja2Templates(directory=os.path.join(BASE_DIR, 'templates'))
 
 @app.get("/")
 async def home(request: Request):
